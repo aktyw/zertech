29f6093e5ac279d1b8d2bbfa1528715c380ac7cb
a.tywonek <arek.tywonek@gmail.com>
2020-12-13 05:06:34 +0100

    add menu, desktop view, change colors, rwd, overhaul hero section, fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,19 @@
 // Scripts
 const hamburger = document.querySelector(".hamburger");
+const hamName = document.querySelector(".hamburger__name")
 const navigation = document.querySelector(".navigation");
 const menu = document.querySelector(".navigation__list");
 const links = document.querySelectorAll(".navigation__item-link");
+// const logo = document.querySelector(".logo__link");
+const logoWhite = document.querySelector(".logo__white");
+const logoBlue = document.querySelector(".logo__blue");
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
     menu.classList.toggle('menu-active');
+    logoWhite.classList.toggle('hide');
+    logoBlue.classList.toggle('hide');
+    hamName.classList.toggle('hamcolor');
+    // logo.classList.toggle('color');
 
 })
